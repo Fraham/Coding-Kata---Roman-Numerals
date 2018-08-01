7767cc8b24288689823bae0fe5703f8c15d4f4aa
gpye <graham.pye@mppglobal.com>
2018-08-01 21:47:07 +0100

    Refactor to use StringBuilder

--- RomanNumerals/RomanNumeralGenerator.cs
+++ RomanNumerals/RomanNumeralGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace RomanNumerals
 {
@@ -6,15 +7,24 @@ namespace RomanNumerals
     {
         public string Generate(int number)
         {
+            var roman = new StringBuilder();
+
             if (number == 5)
             {
-                return "V";
+                roman.Append("V");
             }
+
             if (number == 2)
             {
-                return "II";
+                roman.Append("II");
             }
-            return "I";
+
+            if (number == 1)
+            {
+                roman.Append("I");
+            }
+
+            return roman.ToString();
         }
     }
 }
